fb310593dbf101d6fcea2dbe74fc5f1a27bb81b6
manmohan.menon <manmohan.menon@gmail.com>
2024-10-21 08:46:02 +0900

    updated code and rectified typo

--- lib/custom_form_generator.rb
+++ lib/custom_form_generator.rb
@@ -4,15 +4,17 @@ require_relative "custom_form_generator/version"
 require 'json'
 require 'slim'
 require 'yaml'
+require 'cgi'
 
 module CustomFormGenerator
   class Error < StandardError; end
+
   class Generator
     def initialize(form_yaml, data_json, config_json, table_yaml)
-      @form_yaml = YAML.load_file(form_yaml)
-      @data_json = JSON.parse(File.read(data_json))
-      @config_json = JSON.parse(File.read(config_json))
-      @table_yaml = YAML.load_file(table_yaml)
+      @form_yaml = load_yaml(form_yaml)
+      @data_json = load_json(data_json)
+      @config_json = load_json(config_json)
+      @table_yaml = load_yaml(table_yaml)
     end
 
     def generate_form
@@ -28,8 +30,19 @@ module CustomFormGenerator
       Slim::Template.new { table_template }.render(self, data: data)
     end
 
-
     private
+
+    def load_yaml(file_path)
+      YAML.load_file(file_path)
+    rescue StandardError => e
+      raise Error, "Failed to load YAML file #{file_path}: #{e.message}"
+    end
+
+    def load_json(file_path)
+      JSON.parse(File.read(file_path))
+    rescue StandardError => e
+      raise Error, "Failed to load JSON file #{file_path}: #{e.message}"
+    end
 
     def form_template
       <<-SLIM
@@ -42,33 +55,42 @@ module CustomFormGenerator
     end
 
     def render_field(field)
+      label = CGI.escapeHTML(field['label'])
+      id = CGI.escapeHTML(field['id'])
+      key = CGI.escapeHTML(field['key'])
+      css_class = CGI.escapeHTML(field['class'])
+      value = CGI.escapeHTML(fetch_nested_value(field['key']).to_s)
+
       case field['type']
       when 'textfield'
-        "<label for='#{field['id']}'>#{field['label']}</label>
-        <input type='text' id='#{field['id']}' class='#{field['class']}' name='#{field['key']}' value='#{fetch_nested_value(field['key'])}' placeholder='Enter your #{field['key']}'/>"
+        "<label for='#{id}'>#{label}</label>
+        <input type='text' id='#{id}' class='#{css_class}' name='#{key}' value='#{value}' placeholder='Enter your #{key}'/>"
       when 'dropdown'
-        value = fetch_nested_value(field['key'])
-        options = value.is_a?(Array) ? value.map { |opt| "<option value='#{opt}'>#{opt}</option>" }.join("\n") : ''
-        "<label for='#{field['id']}'>#{field['label']}</label>
-        <select id='#{field['id']}' class='#{field['class']}' name='#{field['key']}'>\n#{options}\n</select>"
+        options = value.is_a?(Array) ? value.map { |opt| "<option value='#{CGI.escapeHTML(opt)}'>#{CGI.escapeHTML(opt)}</option>" }.join("\n") : ''
+        "<label for='#{id}'>#{label}</label>
+        <select id='#{id}' class='#{css_class}' name='#{key}'>\n#{options}\n</select>"
       when 'datetime-local'
         disabled_attr = field['disabled'] ? 'disabled' : ''
-        "<label for='#{field['id']}'>#{field['label']}</label>
-        <input type='datetime-local' id='#{field['id']}' class='#{field['class']}' name='#{field['key']}' value='#{fetch_nested_value(field['key'])}' #{disabled_attr} />"
+        "<label for='#{id}'>#{label}</label>
+        <input type='datetime-local' id='#{id}' class='#{css_class}' name='#{key}' value='#{value}' #{disabled_attr} />"
       when 'radio'
-        options = field['options'].map { |opt| "<input type='radio' id='#{field['id']}_#{opt['value']}' class='#{field['class']}' name='#{field['key']}' value='#{opt['value']}'><label for='#{field['id']}_#{opt['value']}'>#{opt['label']}</label>" }.join("<br>")
+        options = field['options'].map { |opt| 
+          opt_value = CGI.escapeHTML(opt['value'])
+          opt_label = CGI.escapeHTML(opt['label'])
+          "<input type='radio' id='#{id}_#{opt_value}' class='#{css_class}' name='#{key}' value='#{opt_value}'><label for='#{id}_#{opt_value}'>#{opt_label}</label>" 
+        }.join("<br>")
         "<fieldset>
-          <legend>#{field['label']}</legend>
+          <legend>#{label}</legend>
           #{options}
         </fieldset>"
       when 'textbox'
-        "<label for='#{field['id']}'>#{field['label']}</label>
-        <textarea id='#{field['id']}' class='#{field['class']}' name='#{field['key']}' placeholder='Enter your #{field['key']}'></textarea>"
+        "<label for='#{id}'>#{label}</label>
+        <textarea id='#{id}' class='#{css_class}' name='#{key}' placeholder='Enter your #{key}'>#{value}</textarea>"
       else
         ''
       end
     end
-    
+
     def fetch_nested_value(key)
       keys = key.split('.')
       value = keys.reduce(@data_json) do |data, k|
@@ -76,9 +98,8 @@ module CustomFormGenerator
         data[k]
       end
       puts "DEBUG: key = #{key}, value = #{value.inspect}"
-      value
+      value.is_a?(Array) ? value : value || ''
     end
-    
 
     def filter_and_sort_template
       <<-SLIM
@@ -106,19 +127,18 @@ module CustomFormGenerator
           button type='submit' Apply
       SLIM
     end
-    
 
     def table_template
       <<-SLIM
         table
           tr
-            - @table_fields_yaml.each do |field|
-              th id='#{field['id']}' class='#{field['class']}'= field['label']
+            - @table_yaml.each do |field|
+              th id='#{CGI.escapeHTML(field['id'])}' class='#{CGI.escapeHTML(field['class'])}'= field['label']
             th Activities
           - data.each do |entry|
             tr
-              - @table_fields_yaml.each do |field|
-                td id='#{field['id']}' class='#{field['class']}'
+              - @table_yaml.each do |field|
+                td id='#{CGI.escapeHTML(field['id'])}' class='#{CGI.escapeHTML(field['class'])}'
                   - value = entry.dig(*field['key'].split('.'))
                   - if field['key'] == 'properties.published_at' && value.nil?
                     = "false"
